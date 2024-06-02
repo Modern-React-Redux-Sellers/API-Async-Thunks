22791f772902e1dff1f92bf3738df9ff7c6ddc87
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-06-02 16:09:26 -0500

    implemented removing user thunk and seperated user item into own component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,9 +1,10 @@
 import {useEffect} from "react";
 import {useSelector} from "react-redux";
-import {fetchUsers, addUser} from "../store";
+import {fetchUsers, addUser, removeUser} from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import {useThunk} from "../hooks/useThunk";
+import UsersListItem from "./UsersListItem";
 
 
 const UsersList = () => {
@@ -23,33 +24,29 @@ const UsersList = () => {
        doCreateUser();
     }
 
+    let content;
+
     if (isLoadingUsers){
-        return <Skeleton times={6} className="h-10 w-full"/>
+        content = <Skeleton times={6} className="h-10 w-full"/>
+    } else if (loadingUsersError){
+        content = <div>Error fetching data</div>
+    } else {
+        content = data.map((user) => {
+            return <UsersListItem user={user} key={user.id}/>
+        })
     }
 
-    if (loadingUsersError){
-        return <div>Error fetching data</div>
-    }
 
-    const renderedUsers = data.map((user) => {
-        return <div key={user.id} className="mb-2 border-rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                {user.name}
-            </div>
-        </div>
-    })
 
     return <div>
-        <div className="flex flex-row justify-between m-3">
+        <div className="flex flex-row justify-between items-center m-3">
             <h1 className="m-2 text-xl">Users</h1>
-            { isCreatingUser ? 'Creating user' :
-                <Button onClick={handleUserAdd}>
+                <Button loading={isCreatingUser} onClick={handleUserAdd}>
                     + Add User
                 </Button>
-            }
             {creatingUserError && 'Error creating user..'}
         </div>
-        {renderedUsers}
+        {content}
     </div>
 }
 
